9494ed87e6632df6f1202d5d4a56cafedad5ba01
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-03-22 00:13:08 +0100

    Add tests for sieve of eratosthenes and spiral generation

--- test/ulamspiral_test.cpp
+++ test/ulamspiral_test.cpp
@@ -1,10 +1,31 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
 #include "ulamspiral.hpp"
 
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 TEST(ulamspiral, shouldComputeCorrectRangeValue) {
-    EXPECT_EQ(ulamspiral::computeNumbersRange(3), 10);
-    EXPECT_EQ(ulamspiral::computeNumbersRange(5), 26);
-    EXPECT_EQ(ulamspiral::computeNumbersRange(3000), 9000001);
+  EXPECT_EQ(ulamspiral::computeNumbersRange(3), 10);
+  EXPECT_EQ(ulamspiral::computeNumbersRange(5), 26);
+  EXPECT_EQ(ulamspiral::computeNumbersRange(3000), 9000001);
+}
+
+TEST(ulamspiral, shouldMarkAllPrimes) {
+  auto sievedPrimes = ulamspiral::sieveOfEratosthenes(4);
+  EXPECT_THAT(*sievedPrimes, testing::ElementsAre(0, 1, 1, 0));
+
+  sievedPrimes = ulamspiral::sieveOfEratosthenes(16);
+  EXPECT_THAT(*sievedPrimes, testing::ElementsAre(0, 1, 1, 0, 1,    // 1, 2, 3, 4, 5
+                                                  0, 1, 0, 0, 0,    // 6, 7, 8, 9, 10
+                                                  1, 0, 1, 0, 0, 0  // 11, 12, 13, 14, 15, 16
+                                                  ));
+}
+
+TEST(ulamspiral, shouldGenerateUlamSpiral) {
+  auto spiral = ulamspiral::makeUlamSpiral(5);
+  EXPECT_THAT(*spiral, testing::ElementsAre(1, 0, 0, 0, 1,  // 17, 16, 15, 14, 13
+                                            0, 1, 0, 1, 0,  // 18,  5,  4,  3, 12
+                                            1, 0, 0, 1, 1,  // 19,  6,  1,  2, 11
+                                            0, 1, 0, 0, 0,  // 20,  7,  8,  9, 10
+                                            0, 0, 1, 0, 0   // 21, 22, 23, 24, 25
+                                            ));
 }
