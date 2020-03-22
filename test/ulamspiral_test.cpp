da4f6abae78dab02b4e075842666d36536c626f0
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-03-22 12:15:42 +0100

    Add test case for even width value in spiral generation

--- test/ulamspiral_test.cpp
+++ test/ulamspiral_test.cpp
@@ -21,11 +21,18 @@ TEST(ulamspiral, shouldMarkAllPrimes) {
 }
 
 TEST(ulamspiral, shouldGenerateUlamSpiral) {
-  auto spiral = ulamspiral::makeUlamSpiral(5);
-  EXPECT_THAT(*spiral, testing::ElementsAre(1, 0, 0, 0, 1,  // 17, 16, 15, 14, 13
-                                            0, 1, 0, 1, 0,  // 18,  5,  4,  3, 12
-                                            1, 0, 0, 1, 1,  // 19,  6,  1,  2, 11
-                                            0, 1, 0, 0, 0,  // 20,  7,  8,  9, 10
-                                            0, 0, 1, 0, 0   // 21, 22, 23, 24, 25
-                                            ));
+  auto oddSpiral = ulamspiral::makeUlamSpiral(5);
+  EXPECT_THAT(*oddSpiral.spiral, testing::ElementsAre(1, 0, 0, 0, 1,  // 17, 16, 15, 14, 13
+                                                      0, 1, 0, 1, 0,  // 18,  5,  4,  3, 12
+                                                      1, 0, 0, 1, 1,  // 19,  6,  1,  2, 11
+                                                      0, 1, 0, 0, 0,  // 20,  7,  8,  9, 10
+                                                      0, 0, 1, 0, 0   // 21, 22, 23, 24, 25
+                                                      ));
+
+  auto evenSpiral = ulamspiral::makeUlamSpiral(4);
+  EXPECT_THAT(*evenSpiral.spiral, testing::ElementsAre(0, 0, 0, 1,  // 16, 15, 14, 13
+                                                       1, 0, 1, 0,  //  5,  4,  3, 12
+                                                       0, 0, 1, 1,  //  6,  1,  2, 11
+                                                       1, 0, 0, 0   //  7,  8,  9, 10
+                                                       ));
 }
