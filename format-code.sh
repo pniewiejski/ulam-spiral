da3efc400cb85dcebff82dfd850e441aea88b1ae
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-08-15 01:53:40 +0200

    Update the C++ formatting script

--- format-code.sh
+++ format-code.sh
@@ -1,5 +1,27 @@
-set -xe
-for DIR in ./src ./include ./test
-do
-    find $DIR -regex '.*\.[ch]p*' -exec clang-format -style=file -i {} \;
-done 
+set -x
+DIRS="./src ./test ./include"
+
+case $1 in
+    test)
+        echo "Checking code formatting..."
+        for DIR in $DIRS
+        do
+            find $DIR -regex '.*\.[ch]p*' -exec clang-format -style=file -output-replacements-xml {} \; |
+            grep -c "<replacement " > /dev/null
+            if [ $? -ne 1 ]; then
+                echo "Commit did not match clang-format"
+                exit 1
+            else
+                echo "No replacements suggested. It looks OK!"
+                exit 0
+            fi
+        done
+        ;;
+    *)
+        echo "Running code formatting..."
+        for DIR in $DIRS
+        do
+            find $DIR -regex '.*\.[ch]p*' -exec clang-format -style=file -i {} \;
+        done
+        ;;
+esac
